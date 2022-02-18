dbef36e56ea99f98969937610373ca190691bf8b
Tornado <tornado@gmail.com>
2022-02-18 07:00:01 +0200

    Implement HTML and CSS and related JS

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 async function getLocations() {
     const locations = {};
-    const url = "https://countriesnow.space/api/v0.1/countries";
+    const url = "https://countriesnow.space/api/v0.1/countries/capital";
     try{
         const response = await fetch(url);
         if (!response.ok) {
@@ -8,7 +8,9 @@ async function getLocations() {
         }
         const data = await response.json();
         data['data'].forEach((element) => {
-            locations[element.country] = element.cities;
+            if(element.capital !== "") {
+                locations[element.name] = [element.capital];
+            }
         });
         return locations;
     } catch(error) {
@@ -56,25 +58,44 @@ async function getWeatherDescriptiveImage(weather) {
         if(responseData.data.length === 0) {
             throw new Error("Image not found");
         }
-        return data;
+        return responseData;
     } catch(error) {
         console.log(error);
     }
 }
 
+const weatherCard = document.querySelector("#section-weather-details .weather-card");
+const descriptionColumn = weatherCard.querySelector(".card-header .details")
+const cityName = weatherCard.querySelector(".card-header h2");
+const description = weatherCard.querySelector(".description");
+const celsiusTemperature = weatherCard.querySelector(".celsius .value");
+const fahrenheitTemperature  = weatherCard.querySelector(".fahrenheit .value");
+const date = weatherCard.querySelector(".date");
+const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const weatherImageContainer = weatherCard.querySelector(".card-body .img-container");
+const weatherImage = weatherImageContainer.querySelector("img");
+const weatherImageSpinner = weatherImageContainer.querySelector(".spinner-container");
+weatherImage.addEventListener("load", () => {
+    weatherImage.classList.remove("loading");
+    weatherImageSpinner.classList.add("d-none");
+});
+
 async function setWeather(city) {
     try{
-        /*  Todo
-                set city element
-                set weather description element
-                set image element source
-                set temperature elements
-        */
         const weather = await getWeather(city);
+        cityName.textContent = city;
+        description.textContent = weather.weather[0].main;
+        celsiusTemperature.textContent = Math.round(weather.main.temp * 10) / 10;
+        fahrenheitTemperature.textContent = Math.round(celsiusToFahrenheit(weather.main.temp) * 10) / 10;
+        const today = new Date();
+        const todaySting = today.toDateString();
+        date.textContent = days[today.getDay()] + ", " + todaySting.substring(todaySting.indexOf(" ") + 1);
+        weatherImageContainer.classList.add("loading");
+        weatherImageSpinner.classList.remove("d-none");
         const imageResponse = await getWeatherDescriptiveImage(weather.weather[0].description);
         const image = imageResponse.data.images.downsized.url;
-        const celsiusTemperature = weather.main.temp;
-        const fahrenheitTemperature = celsiusToFahrenheit(celsiusTemperature);
+        weatherImage.setAttribute("src", image);
+        descriptionColumn.classList.remove("d-none");
     } catch (error) {
         console.log(error);
     }
@@ -83,3 +104,36 @@ async function setWeather(city) {
 function celsiusToFahrenheit(temperature) {
     return (temperature * 9/5) + 32
 }
+
+
+const cities = document.querySelector(".city-choose select");
+cities.addEventListener("change", () => {
+    setWeather(cities.value);
+})
+
+let locations = getLocations().then((data) => {
+    locations = data;
+    Object.keys(locations).forEach((country) => {
+        cities.append(createCountyOptions(country, locations[country]));
+    });
+});
+
+
+function createCountyOptions(country, cities){
+    const optGroup = document.createElement("optgroup");
+    optGroup.setAttribute("label", country);
+    cities.forEach((city) => {
+        optGroup.append(createCityOption(city));
+    });
+    return optGroup;
+}
+
+function createCityOption(city){
+    const option = document.createElement("option");
+    option.setAttribute("value", city);
+    option.textContent = city;
+    return option;
+}
+
+
+
